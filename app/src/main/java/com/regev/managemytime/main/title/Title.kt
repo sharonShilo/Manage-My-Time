6d66f478d43076e3bbbba66998b3c45d18f9e9aa
SH-WORK\shilo <shiloregev1@gmail.com>
2025-06-29 16:41:26 +0300

    work on title

---
+++ app/src/main/java/com/regev/managemytime/main/title/Title.kt
@@ -0,0 +1,108 @@
+package com.regev.managemytime.main.title
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.regev.managemytime.R
+
+@Composable
+fun MainTitle() {
+    Row(modifier = Modifier.fillMaxWidth()
+        .height(100.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        ImageAndTitle()
+        ButtonsForTitle()
+    }
+}
+
+@Composable
+fun ButtonsForTitle() {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(100.dp)
+    ) {
+//        CircularButton()
+//        CircularButton()
+    }
+}
+
+@Composable
+fun CircularButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    iconTint: Color = Color.White,
+    backgroundColor: Color = Color.Black.copy(alpha = 0.3f)
+) {
+    Box(
+        modifier = modifier
+            .size(48.dp)
+            .clip(CircleShape)
+            .background(backgroundColor)
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = iconTint,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+
+}
+
+@Composable
+fun ImageAndTitle() {
+    Row(modifier = Modifier.
+        width(60.dp)
+        .padding(horizontal = 10.dp)
+        .fillMaxHeight()) {
+        CircularImage(
+            imageResource = R.drawable.ic_launcher_foreground,
+            contentDescription = "Profile picture",
+            size = 120,
+            borderWidth = 3f,
+            borderColor = Color.Black
+        )
+        Spacer(modifier = Modifier
+            .fillMaxHeight()
+            .width(10.dp)
+        )
+    }
+}
+
+@Composable
+fun CircularImage(
+    imageResource: Int,
+    contentDescription: String? = null,
+    size: Int = 100,
+    borderWidth: Float = 2f,
+    borderColor: Color = Color.Gray
+    ) {
+    Image(
+        painter = painterResource(id = imageResource),
+        contentDescription = contentDescription,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(size.dp)
+            .clip(CircleShape)
+            .border(borderWidth.dp, borderColor, CircleShape)
+
+    )
+}
