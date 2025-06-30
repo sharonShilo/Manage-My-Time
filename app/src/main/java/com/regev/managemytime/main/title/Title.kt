1d9cb69f940b2581a62f68478cf649403e580a36
shilo <shiloregev1@gmail.com>
2025-06-30 16:06:43 +0300

    continure main title

--- app/src/main/java/com/regev/managemytime/main/title/Title.kt
+++ app/src/main/java/com/regev/managemytime/main/title/Title.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,25 +20,46 @@ import androidx.compose.ui.unit.dp
 import com.regev.managemytime.R
 
 @Composable
-fun MainTitle() {
+fun MainTitle(
+    imageResource: Int,
+    greeting: String,
+    name: String,
+    firstButtonIcon: ImageVector,
+    firstButtonOnClick: () -> Unit,
+    secondButtonIcon: ImageVector,
+    secondButtonOnClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    ) {
     Row(modifier = Modifier.fillMaxWidth()
         .height(100.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        ImageAndTitle()
-        ButtonsForTitle()
+        ImageAndTitle(imageResource, greeting, name)
+        ButtonsForTitle(firstButtonIcon, firstButtonOnClick, secondButtonIcon, secondButtonOnClick, modifier)
     }
 }
 
 @Composable
-fun ButtonsForTitle() {
-    Row(
-        modifier = Modifier
+fun ImageAndTitle(imageResource: Int, greeting: String, name: String) {
+    Row(modifier = Modifier.
+    width(60.dp)
+        .padding(horizontal = 10.dp)
+        .fillMaxHeight()) {
+        CircularImage(
+            imageResource = imageResource,
+            contentDescription = "Profile picture",
+            size = 120,
+            borderWidth = 3f,
+            borderColor = Color.Black
+        )
+        Spacer(modifier = Modifier
             .fillMaxHeight()
-            .width(100.dp)
-    ) {
-//        CircularButton()
-//        CircularButton()
+            .width(10.dp)
+        )
+        Column {
+            Text(greeting)
+            Text(name)
+        }
     }
 }
 
@@ -68,26 +90,6 @@ fun CircularButton(
 }
 
 @Composable
-fun ImageAndTitle() {
-    Row(modifier = Modifier.
-        width(60.dp)
-        .padding(horizontal = 10.dp)
-        .fillMaxHeight()) {
-        CircularImage(
-            imageResource = R.drawable.ic_launcher_foreground,
-            contentDescription = "Profile picture",
-            size = 120,
-            borderWidth = 3f,
-            borderColor = Color.Black
-        )
-        Spacer(modifier = Modifier
-            .fillMaxHeight()
-            .width(10.dp)
-        )
-    }
-}
-
-@Composable
 fun CircularImage(
     imageResource: Int,
     contentDescription: String? = null,
@@ -105,4 +107,28 @@ fun CircularImage(
             .border(borderWidth.dp, borderColor, CircleShape)
 
     )
+}
+
+@Composable
+fun ButtonsForTitle( firstButtonIcon: ImageVector,
+                     firstButtonOnClick: () -> Unit,
+                     secondButtonIcon: ImageVector,
+                     secondButtonOnClick: () -> Unit,
+                     modifier: Modifier = Modifier,) {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(100.dp)
+    ) {
+        CircularButton(
+            icon =  firstButtonIcon,
+            onClick = firstButtonOnClick,
+            modifier = modifier
+        )
+        CircularButton(
+            icon = secondButtonIcon,
+            onClick = secondButtonOnClick,
+            modifier
+        )
+    }
 }
